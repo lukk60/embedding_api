8c01cc46472475249731fe2cefdc89c3824f7d68
lukk60 <lukk60@gmail.com>
2019-11-03 15:54:31 +0100

    tt

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ class FlaskRestApi(Resource):
 api.add_resource(FlaskRestApi, '/embapi', endpoint = 'flaskrest')
 
 if __name__ == '__main__':
-    app.run(debug=True,  port=8000)
+    app.run(debug=True,  port=8000)  
